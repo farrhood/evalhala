b42a0e75f9d75d9337ab481b7c6ff9201e36b5ec
Farhood Fazeli <farhood.fazeli@gmail.com>
2017-09-19 11:03:05 +0200

    initial commit

---
+++ src/main/java/ninja/farhood/evalhala/EvalhalaApplication.java
@@ -0,0 +1,12 @@
+package ninja.farhood.evalhala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalhalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalhalaApplication.class, args);
+	}
+}
