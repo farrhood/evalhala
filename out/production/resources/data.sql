769becad2f7df245d43741364c07c5efe5c9e9ed
Farhood Fazeli <farhood.fazeli@gmail.com>
2017-09-22 15:27:26 +0200

    saving progress

--- out/production/resources/data.sql
+++ out/production/resources/data.sql
@@ -1,4 +1,4 @@
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1000, 'Git', 'geen', 5, 5, 4, 5, 2, 3, 5, 5, 4);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1110, 'Git', 'geen', 5, 5, 4, 5, 2, 3, 5, 5, 4);
 insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1111, 'Java Programming', 'geen', 5, 5, 4, 5, 2, 3, 5, 5, 4);
 insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1112, 'Sollicitatietraining', 'Geen les in sollicitatietraining dus meeste vragen zijn hier niet van toepassing. Ben over het algemeen tevreden met Roel zijn deel van het traject.', 5, 5, 4, 5, 2, 3, 5, 5, 2);
 insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1113, 'Gradle & unit testing with Java', 'geen', 2, 1, 3, 5, 2, 3, 5, 1, 4);
@@ -12,5 +12,4 @@ insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7,
 insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1121, 'JavaScript & ES6 ', 'geen', 5, 5, 5, 5, 5, 5, 5, 5, 5);
 insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1122, 'Jquery Mobile ', 'jQuery is verplaatst naar morgen 6 september.', 5, 5, 5, 5, 5, 5, 5, 5, 5);
 
-insert into evaluatie(id, voornaam, achternaam, tewerkstellingskansen, bedrijvenlijst, algemenefeedback) values(1, 'Jimmy', 'Neutron', 'Zeer goede kans op tewerkstelling.', 'NASA', 'Misschien meer oefeningen en praktijk, soms te veel theorie en dan snel naar het volgend onderwerp voor meer theorie.');
-
+insert into evaluatie(id, voornaam, achternaam, tewerkstellingskansen, bedrijvenlijst, algemenefeedback) values(1, 'Jimmy', 'Neutron', 'Zeer goede kans op tewerkstelling.', 'Stripe, Hubspot, Apple', 'Misschien meer oefeningen en praktijk, soms te veel theorie en dan snel naar het volgend onderwerp voor meer theorie.');
