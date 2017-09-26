9a27dff1529c690781068abec3ec47b2b4252d91
Farhood Fazeli <farhood.fazeli@gmail.com>
2017-09-26 11:29:02 +0200

    saving progress

--- out/production/resources/data.sql
+++ out/production/resources/data.sql
@@ -1,15 +1,16 @@
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1110, 'Git', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1111, 'Java Programming', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1112, 'Sollicitatietraining', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1113, 'Gradle & unit testing with Java', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1114, 'Communicatietraining', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1115, 'SQL queries', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1116, 'Persistence in Java', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1117, 'GOF Design Patterns', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1118, 'Working with HTML5 and CSS', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1119, 'Response webdesign', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1120, 'SASS/Less', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1121, 'JavaScript & ES6 ', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9) values(1122, 'Jquery Mobile ', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0);
-
 insert into evaluatie(id, voornaam, achternaam, tewerkstellingskansen, bedrijvenlijst, algemenefeedback) values(1, 'Jimmy', 'Neutron', 'test', 'test', 'test');
+
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1110, 'Git', 'Git is magic.', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1111, 'Java Programming', 'Kevin is mijn favoriete leraar in de hele wereld.', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1112, 'Sollicitatietraining', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1113, 'Gradle & unit testing with Java', 'Despacito.', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1114, 'Communicatietraining', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1115, 'SQL queries', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1116, 'Persistence in Java', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1117, 'GOF Design Patterns', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1118, 'Working with HTML5 and CSS', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1119, 'Responsive webdesign', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1120, 'SASS/Less', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1121, 'JavaScript & ES6', 'Kevin is de allercoolste trainer in de hele wereld.', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+insert into cursus_eval(id, cursusnaam, opmerkingen, q1, q2, q3, q4, q5, q6, q7, q8, q9, evaluatie_id) values(1122, 'Jquery Mobile', 'test', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+
